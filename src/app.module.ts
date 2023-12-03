e7ceeaf481805cf8d54e83ffd8eaf0747f1af624
kiwonbyun <bkw9603@naver.com>
2023-12-04 00:05:26 +0900

    connect typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  ENV_DB_DATABASE,
+  ENV_DB_HOST,
+  ENV_DB_PASSWORD,
+  ENV_DB_PORT,
+  ENV_DB_USERNAME,
+} from './common/constants/env-key.const';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env[ENV_DB_HOST],
+      port: +process.env[ENV_DB_PORT],
+      username: 'babylon-kiwon',
+      password: 'babylon-qposf38',
+      database: 'babylon-project',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/constants/env-key.const.ts
@@ -0,0 +1,5 @@
+export const ENV_DB_HOST = 'DB_HOST';
+export const ENV_DB_PORT = 'DB_PORT';
+export const ENV_DB_USERNAME = 'DB_USERNAME';
+export const ENV_DB_PASSWORD = 'DB_PASSWORD';
+export const ENV_DB_DATABASE = 'DB_DATABASE';
