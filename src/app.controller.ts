72450a6ab3185d6418a2fb9658a068ed20315e1e
kiwonbyun <bkw9603@naver.com>
2024-02-08 11:42:07 +0900

    cicd 테스트용 배포

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): boolean {
+  getHello() {
     return this.appService.heartbeat();
   }
 }
